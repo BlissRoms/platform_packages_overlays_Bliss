bccc8dc2b4aa098c3aa3e416674519b953aaa12b
Naveen Johnson <14140949+naveenjohnsonv@users.noreply.github.com>
2024-05-27 02:34:59 +0900

    overlays: Add OneUISans fonts
    Change-Id: I46c6cb55e36546a40b36da76a7014be2151a876c
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- bliss_packages.mk
+++ bliss_packages.mk
@@ -42,6 +42,7 @@ PRODUCT_PACKAGES += \
     FontNothingDotHeadlineOverlay \
     FontNothingDotOverlay \
     FontNunitoOverlay \
+    FontOneUISansOverlay \
     FontOneplusSansOverlay \
     FontOneplusSlateOverlay \
     FontOswaldOverlay \
