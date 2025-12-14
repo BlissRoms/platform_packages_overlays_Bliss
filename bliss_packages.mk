1e794eb55ba1a45be505b00966b7ee83cef3d42a
Jis G Jacob <studiokeys@blissroms.org>
2025-12-14 07:29:51 -0500

    overlays: Import fonts and SystemUIClocks from `waterlily` branches
    Change-Id: Icc42ad8968d0bcebbe8a044fa49df6e9b00cb359
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ bliss_packages.mk
@@ -0,0 +1,68 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#               2017-2024 crDroid Android Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Clocks (SystemUI)
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml \
+    FontAccuratistOverlay \
+    FontAclonicaOverlay \
+    FontAmaranteOverlay \
+    FontBariolOverlay \
+    FontCagliostroOverlay \
+    FontCoconOverlay \
+    FontComfortaaOverlay \
+    FontComicSansOverlay \
+    FontCookieRunOverlay \
+    FontCooljazzOverlay \
+    FontCoolstoryOverlay \
+    FontExotwoOverlay \
+    FontEvoSansOverlay \
+    FontEvolveSansOverlay \
+    FontFifa2018Overlay \
+    FontGrandHotelOverlay \
+    FontGeneralSansOverlay \
+    FontGoogleSansFlexOverlay \
+    FontHarmonySansOverlay \
+    FontIBMPlexSansOverlay \
+    FontLatoOverlay \
+    FontLinotteOverlay \
+    FontNokiaPureOverlay \
+    FontNothingDotHeadlineOverlay \
+    FontNothingDotOverlay \
+    FontNunitoOverlay \
+    FontOneUISansOverlay \
+    FontOneplusSansOverlay \
+    FontOneplusSlateOverlay \
+    FontPoppinsSourceOverlay \
+    FontOswaldOverlay \
+    FontPlayOverlay \
+    FontQuandoOverlay \
+    FontRedressedOverlay \
+    FontReemKufiOverlay \
+    FontRobotoCondensedOverlay \
+    FontRookeryOverlay \
+    FontRubikOverlay \
+    FontSamsungOneOverlay \
+    FontSanFranciscoDisplayProSourceOverlay \
+    FontSansSerifOverlay \
+    FontSansSerifProOverlay \
+    FontSonySketchOverlay \
+    FontSpaceGroteskOverlay \
+    FontStoropiaOverlay \
+    FontSurferOverlay \
+    FontUbuntuOverlay \
+    FontVolteOverlay
