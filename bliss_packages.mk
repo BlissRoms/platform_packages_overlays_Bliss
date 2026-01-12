2d262aa608ba92560d75ce98aac059d965656a73
Jis G Jacob <studiokeys@blissroms.org>
2026-01-12 01:59:21 -0500

    overlays: Import SystemUIClocks from `waterlily` branches
    Change-Id: Ie41965001558067a32aa1f9ca10d5984386b6f84
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ bliss_packages.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2018 The LineageOS Project
+#               2017-2024 crDroid Android Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Clocks (SystemUI)
+PRODUCT_PACKAGES += \
+    SystemUIClocks-BigNum \
+    SystemUIClocks-Calligraphy \
+    SystemUIClocks-Flex \
+    SystemUIClocks-Growth \
+    SystemUIClocks-Inflate \
+    SystemUIClocks-Metro \
+    SystemUIClocks-NumOverlap
+
+# Fonts
+PRODUCT_PACKAGES += \
+    fonts_customization.xml
